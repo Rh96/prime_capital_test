e82eef525b984f2fb68a2023f086f8e8120a7e89
Rh96 <radehrgo96@gmail.com>
2026-09-04 19:30:55 +0200

    resolving conflicts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
+<<<<<<< HEAD
 Route::get('/', function () {
     return view('welcome');
 });
+=======
+Route::redirect('/', '/dashboard');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
+>>>>>>> cd7ddb7 (chore: initialize laravel project with breeze livewire stack)
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,10 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
+<<<<<<< HEAD
         $response->assertStatus(200);
+=======
+        $response->assertRedirect('/dashboard');
+>>>>>>> cd7ddb7 (chore: initialize laravel project with breeze livewire stack)
     }
 }
